c097ecb993f8b094b0e2a8e8c5a5cecaf5472891
Peter Crona <petercrona89@gmail.com>
2017-11-26 10:57:21 +0100

    readme and HOC

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # mvsplit-react
 A React HOC that make Mvsplit and React a pleasure to use together
+
+Mvsplit can be a bit scary, at least if you are not familiar with functors.
+Mvsplit-React provides you with a nice little HOC that allows you to express your
+React code more clearly. Let's look at an example:
+
+``` JavaScript
+// Model
+export const create = () => ({counter: 1});
+export const inc = ({counter}) => ({counter: counter + 1});
+export const dec = ({counter}) => ({counter: counter - 1});
+
+// Component
+import {create, dec, inc} from './Model';
+
+function Counter({refunc: {model, notify}}) {
+    return (
+        <div>
+            <p>
+                <strong>Counter is {model.counter}</strong>
+            </p>
+            <p>
+                <button onClick={notify(dec)}>Decrement</button>
+                <button onClick={notify(inc)}>Increment</button>
+                <button onClick={notify(inc, dec, inc, dec)}>Silly button</button>
+            </p>
+        </div>
+    );
+}
+
+export default observe(create(), Counter);
+```
+
+If you used Redux, you're probably already familiar with this pattern.
+"observe" will ensure that your component is updated every time notify is called.
+
+Cheers,
+Peter Crona
---
+++ index.js
@@ -0,0 +1,47 @@
+const React = require('react');
+const Observable = require('mvsplit');
+
+const curry = (f) => (...args) => {
+    const nrArgsRequired = f.length;
+    if (args.length < nrArgsRequired) {
+        return curry(f.bind(null, ...args));
+    }
+    return f(...args);
+};
+const map = curry((fn, xs) => xs.map(fn));
+const tail = xs => xs.slice(1, xs.length);
+const head = xs => xs[0];
+const compose = (...fns) => (...args) =>
+    reduce((m, f) => f(m), last(fns)(...args), tail(reverse(fns)));
+
+module.exports = curry((model, ComponentToWrap) => {
+    return class WithObserver extends Component {
+        constructor(props) {
+            super(props);
+
+            let observable = Observable.mkObservable(model);
+            observable = Observable.observe(observable, (newObservable) => {
+                this.setState({observable: newObservable});
+            });
+
+            this.state = {
+                observable
+            };
+        }
+
+        render() {
+            const handle = (...fns) => () =>
+                compose(head(fns), ...map(Observable.fmap, tail(fns)))(this.state.observable);
+            const notify = (...fns) => () =>
+                compose(Observable.notify, ...map(Observable.fmap, fns))(this.state.observable);
+            const mvsplitProps = {
+                handle,
+                notify,
+                model: Observable.getModel(this.state.observable),
+                observable: this.state.observable
+            };
+
+            return React.createElement(ComponentToWrap, {...this.props, mvsplit: mvsplitProps});
+        }
+    };
+});
